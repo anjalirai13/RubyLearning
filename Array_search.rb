cca2665190fa159cf437cdf0490ee4b09827453f
mranjali <anjali.rai23@gmail.com>
2016-09-14 20:59:15 +0530

    array search

--- Array_search.rb
+++ Array_search.rb
@@ -6,8 +6,8 @@
 
 puts "For how many iterations you want to do the search"
 p t = gets.to_i
-if (t == 0 || t >= 100)
-	puts "Please provide number as input ranging from 1 to 99"
+if (t == 0 || t >= 100 || t <= 0)
+	puts "Please provide no of iterations ranging from 1 to 99"
 	t = gets.to_i
 end
 result_array = []
@@ -19,16 +19,16 @@ for i in 0...t
 		if (t < 100)
 			puts "Enter the size of array"
 			n = gets.to_i
-			if (n == 0 || n >= 100)
-				puts "Please provide number as input ranging from 1 to 99"
+			if (n == 0 || n >= 100 || n < 0)
+				puts "Please provide input of array size ranging from 1 to 99"
 				n = gets.to_i
 			end
 			if (n != 0)
-				if (n < 100)
+				if (n < 100 || n < 0)
 					puts "Enter the array elements one by one"
 					for k in 0...n
 						search_array << gets.to_i
-						until (search_array.last <= 100)
+						until (search_array.last <= 100 && search_array.last > 0)
 							puts "You have entered outside of range. Enter again"
 							search_array.pop
 							search_array << gets.to_i
@@ -36,7 +36,7 @@ for i in 0...t
 					end
 					puts "Enter the value you want to search"
 					search_value = gets.to_i
-					until (search_value <= 100)
+					until (search_value <= 100 && search_value < 0)
 						puts "You have entered outside of range. Enter again"
 						search_value = nil
 						search_value = gets.to_i
